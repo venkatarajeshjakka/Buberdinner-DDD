56951b650f980162c4fd358fdbc9e6bf9c38d705
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2023-10-23 23:31:19 +0530

    Added jwt generator

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,15 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
     public AuthenticationResult Login(string email, string password)
     {
         return new AuthenticationResult(
@@ -18,11 +26,21 @@ public class AuthenticationService : IAuthenticationService
         string email,
         string password)
     {
+
+        //Check if user already exists
+
+        //Create user ( generate unique ID)
+
+        //Create JWT token
+        Guid userId = Guid.NewGuid();
+        
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
         return new AuthenticationResult(
-            Guid.NewGuid(),
+            userId,
             firstName,
             lastName,
             email,
-            "token");
+            token);
     }
 }
