74abfca24ad10cff779c69da0fc3ffcc3dbc6cd3
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2023-10-28 20:38:10 +0530

    Added Exception handler

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,7 @@
-using BuberDinner.Api.Middleware;
+using BuberDinner.Api.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,13 +9,20 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
 
+    //builder.Services.AddControllers(options =>
+    //options.Filters.Add<ErrorHandlingFilterAttribute>());
+
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
 
-    app.UseMiddleware<ErrorHandlingMiddleware>();
+    //app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
+
     app.UseHttpsRedirection();
 
     app.MapControllers();
